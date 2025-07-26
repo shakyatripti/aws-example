88aa5d2fc4311886187fa06cf16ae64c017f262a
Tripti <shakyatripti1308@gmail.com>
2025-07-26 15:26:38 +0530

    Initialized aws project into github

---
+++ src/main/java/org/example/dto/Course.java
@@ -0,0 +1,15 @@
+package org.example.dto;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+    private int id;
+    private String name;
+    private double price;
+}
